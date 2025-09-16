b985483cf89d7bd0b8e5750df732993259421349
Iaroslav Gurskyi <umlprog@gmail.com>
2025-09-16 21:20:53 +0300

    JobProducerService - file upload endpoint

--- JobProducerService/Controllers/JobPublisherController.cs
+++ JobProducerService/Controllers/JobPublisherController.cs
@@ -21,15 +21,40 @@ namespace JobProducerService.Controllers
             this.sendEndpointProvider = sendEndpointProvider;
         }
 
-        [HttpPost(Name = "PostJob")]
+        [HttpPost]
+        [Route("create-job")]
         public async Task<ActionResult> Post(JobRequest jobRequest)
         {
-            
             var uri = $"queue:{this.appSettings.Value.RabbitConfig.QueueName}";
             var endpoint = await sendEndpointProvider.GetSendEndpoint(new Uri(uri));
             await endpoint.Send(jobRequest);
 
             return Accepted(jobRequest.JobId);
+        }
+
+        [HttpPost]
+        [Route("create-job-file")]
+        public async Task<ActionResult> Post(Guid jobId, JobType type, IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No file uploaded.");
+            }
+
+            var request = new JobRequest { JobId = jobId, Type = type };
+            using (var stream = new MemoryStream())
+            {
+                await file.CopyToAsync(stream);
+                stream.Position = 0;
+                var base64FileContent = Convert.ToBase64String(stream.GetBuffer());
+
+                return await Post(new JobRequest
+                {
+                    JobId = request.JobId, 
+                    Payload = new JobPayload(base64FileContent), 
+                    Type = request.Type
+                });
+            }
         }
     }
 }
