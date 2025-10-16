36edbe122ec901cc7ca3e1f60df53ce622b010d5
Iaroslav Gurskyi <umlprog@gmail.com>
2025-10-16 21:05:45 +0300

    First front end auth with keycloack!

--- JobManagerReactApp/src/App.tsx
+++ JobManagerReactApp/src/App.tsx
@@ -1,37 +1,57 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import RegisteredJobsView from './components/RegisteredJobsView'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
 import NewJobSelector from './components/NewJobSelector'
 import JobDetailsView from './components/JobDetailsView'
-
+import {  useKeycloak } from '@react-keycloak/web';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import './App.css'
 
 function App() {
+    const { keycloak, initialized } = useKeycloak();
     return (
         <BrowserRouter>
             <Navbar bg="dark" variant="dark" expand="lg">
                 <Container>
                     <Navbar.Brand><i className="bi bi-leaf-fill text-info"> </i>Tasks Manager</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <LinkContainer to="/">
-                            <Nav.Link>All Tasks</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/new">
-                            <Nav.Link>Create New Task</Nav.Link>
-                        </LinkContainer>
-                    </Nav>
-                </Container>
-            </Navbar>
+                    {initialized && keycloak.authenticated &&
+                        <>
+                            <Nav className="me-auto">
+                                <LinkContainer to="/">
+                                    <Nav.Link>All Tasks</Nav.Link>
+                                </LinkContainer>
+                                <LinkContainer to="/new">
+                                    <Nav.Link>Create New Task</Nav.Link>
+                                </LinkContainer>
+                            </Nav>
+                        </>}
+                     <Nav className="ms-auto">
+                        {!keycloak.authenticated &&
+                            <Button onClick={() => keycloak.login()}> Login</Button>}
+                        {keycloak.authenticated &&
+                            <>
+                                Hello, {keycloak.tokenParsed!.name}
+                                <Button onClick={() => keycloak.logout()}>Logout ({keycloak.tokenParsed!.preferred_username})</Button>
+                            </>}
+                     </Nav>
+                    </Container>
+                </Navbar>
 
-            <Container className="mt-4">
-                <Routes>
-                    <Route path="/" element={<RegisteredJobsView />} />
-                    <Route path="/new" element={<NewJobSelector />} />
-                    <Route path="/jobs/:jobId" element={<JobDetailsView />} />
-                </Routes>
-            </Container>
+                <Container className="mt-4">
+                    {initialized && keycloak.authenticated &&
+                    <Routes>
+                        <Route path="/" element={<RegisteredJobsView />} />
+                        <Route path="/new" element={<NewJobSelector />} />
+                        <Route path="/jobs/:jobId" element={<JobDetailsView />} />
+                    </Routes>}
+                    {!keycloak.authenticated &&
+                    <Card border='warning' className='fs-4  text-center'>
+                        <Card.Body>Currently you are not logged in. Please use Login button at the top right menu.</Card.Body>
+                    </Card>}
+                </Container>
         </BrowserRouter>
   )
 }
--- JobManagerReactApp/src/main.tsx
+++ JobManagerReactApp/src/main.tsx
@@ -1,11 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Keycloak from 'keycloak-js'
+import { ReactKeycloakProvider } from "@react-keycloak/web";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import App from './App.tsx'
 
+const keycloakProv = new Keycloak({
+    url: `${import.meta.env.VITE_KEYCLOAK_ENDPOINT}`,
+    realm: import.meta.env.VITE_KEYCLOAK_REALM,
+    clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
+});
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
+    <StrictMode>
+        <ReactKeycloakProvider authClient={keycloakProv}>
+            <App />
+        </ReactKeycloakProvider>
   </StrictMode>,
 )
